0f9627b566885fccd77a513e3b5954bb98133037
Ayumi Koujin <rough.god35@gmail.com>
2025-07-16 11:31:12 +0900

    Add an app with no content

---
+++ EmployeePurchaseApp/EmployeePurchaseApp/EmployeePurchaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeePurchaseAppApp.swift
+//  EmployeePurchaseApp
+//
+//  Created by Ayumi Koujin on 2025/07/15.
+//
+
+import SwiftUI
+
+@main
+struct EmployeePurchaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmployeePurchaseApp/EmployeePurchaseAppTests/EmployeePurchaseAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  EmployeePurchaseAppTests.swift
+//  EmployeePurchaseAppTests
+//
+//  Created by Ayumi Koujin on 2025/07/15.
+//
+
+import Testing
+
+struct EmployeePurchaseAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
